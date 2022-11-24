427715d29d9b659ff18e03c4eb2814a615f0b644
baygut <65204661+baygut@users.noreply.github.com>
2022-11-24 15:33:08 +0300

    spring initializer files

---
+++ src/main/java/com/baygut/currencyService/CurrencyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.baygut.currencyService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baygut/currencyService/CurrencyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baygut.currencyService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
